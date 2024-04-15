bbfccf73c52330dc85c6a3e0ed841b90174b280f
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 16:23:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Q106
+// this function chck if a year is a leap year
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+console.log(isLeapYear(2020)); // true
+console.log(isLeapYear(2021)); // false
